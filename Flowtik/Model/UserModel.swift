65beda7b76871c98f6a9cc1abaa321bc0df95864
yash <ysodavadiya@gmail.com>
2025-07-24 12:52:52 +0530

    create user Model

---
+++ Flowtik/Model/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Flowtik
+//
+//  Created by Batch1 on 24/07/25.
+//
+
+import Foundation
+
+class UserModel: Identifiable,Codable {
+    var userId: Int
+    var userName: String
+    
+}
