cf4eb0809018308a95f934c3c366fb3c45fb99f8
yash <ysodavadiya@gmail.com>
2025-07-24 12:27:38 +0530

    Initial Commit

---
+++ Flowtik/FlowtikApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowtikApp.swift
+//  Flowtik
+//
+//  Created by Batch1 on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowtikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
