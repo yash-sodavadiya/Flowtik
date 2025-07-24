9210398b6aab2bfa6f1ab42783e4ccba5843a60d
yash <ysodavadiya@gmail.com>
2025-07-24 12:48:37 +0530

    this is create structure of app

---
+++ Flowtik/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Flowtik
+//
+//  Created by Batch1 on 24/07/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
